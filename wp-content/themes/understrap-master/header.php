cbc2979e189c37936b453569f9a26b46b1bd3eac
ChaseMarcum <chase.m.marcum@gmail.com>
2020-03-03 19:26:39 -0800

    Testing the header and sass compiler

--- wp-content/themes/understrap-master/header.php
+++ wp-content/themes/understrap-master/header.php
@@ -17,7 +17,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
@@ -28,7 +27,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<!-- ******************* The Navbar Area ******************* -->
 	<div id="wrapper-navbar">
 
-		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+		<a class="skip-link sr-only sr-only-focusable"
+		   href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
 		<nav id="main-nav" class="navbar navbar-expand-md navbar-dark bg-primary" aria-labelledby="main-nav-label">
 
@@ -36,28 +36,34 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
 			</h2>
 
-		<?php if ( 'container' === $container ) : ?>
+			<?php if ( 'container' === $container ) : ?>
 			<div class="container">
-		<?php endif; ?>
+				<?php endif; ?>
 
-					<!-- Your site title as branding in the menu -->
-					<?php if ( ! has_custom_logo() ) { ?>
+				<!-- Your site title as branding in the menu -->
+				<?php if ( ! has_custom_logo() ) { ?>
 
-						<?php if ( is_front_page() && is_home() ) : ?>
+			<?php if ( is_front_page() && is_home() ) : ?>
 
-							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+				                                 title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+				                                 itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
 
-						<?php else : ?>
+			<?php else : ?>
 
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+				<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"
+				   title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+				   itemprop="url"><?php bloginfo( 'name' ); ?></a>
 
-						<?php endif; ?>
+			<?php endif; ?>
 
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
+				<?php } else {
+					the_custom_logo();
+				} ?><!-- end custom logo -->
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+				        aria-controls="navbarNavDropdown" aria-expanded="false"
+				        aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 
@@ -74,9 +80,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
 					)
 				); ?>
-			<?php if ( 'container' === $container ) : ?>
+				<?php if ( 'container' === $container ) : ?>
 			</div><!-- .container -->
-			<?php endif; ?>
+		<?php endif; ?>
 
 		</nav><!-- .site-navigation -->
 
